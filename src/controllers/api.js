115f5dd7be826adfcc3ab53199d83b6c062a5b5c
penn <gengpei1985@gmail.com>
2020-08-31 00:29:58 -0400

    fix: sharp 0.26.0 bug for png

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -93,7 +93,6 @@ apiController.loadConfig = async function (req) {
 	config.topicSearchEnabled = settings.topicSearchEnabled || false;
 	config.bootswatchSkin = (meta.config.disableCustomUserSkins !== 1 && settings.bootswatchSkin && settings.bootswatchSkin !== '') ? settings.bootswatchSkin : '';
 	config = await plugins.fireHook('filter:config.get', config);
-	console.log(config.csrf_token);
 	return config;
 };
 
